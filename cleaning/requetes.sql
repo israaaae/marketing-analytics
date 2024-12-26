0b585d3abe30a2e0caaa6e8682df1b3b12d6f5d3
Israe KHOUI <152700591+israaaae@users.noreply.github.com>
2024-12-26 08:23:33 +0100

    Update requetes.sql

--- cleaning/requetes.sql
+++ cleaning/requetes.sql
@@ -1,4 +1,4 @@
-
+--1 jointure left entre les 2 tables customers et geography 
 select * from MarketingAnalytics.dbo.customers;
 --------------------------------------------------
 select * from MarketingAnalytics.dbo.geography;
@@ -6,3 +6,4 @@ select c.CustomerID, c.CustomerName, c.Email, c.Gender, c.Age, g.Country, g.City
 from MarketingAnalytics.dbo.customers as c left join MarketingAnalytics.dbo.geography as g
 on c.GeographyID = g.GeographyID
 
+--2 
