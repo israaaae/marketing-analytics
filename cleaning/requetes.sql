ce16b822363d5a1b37433b0d2e66601193d6f964
Israe KHOUI <152700591+israaaae@users.noreply.github.com>
2024-12-26 08:20:49 +0100

    Update and rename requetes sql to requetes.sql

---
+++ cleaning/requetes.sql
@@ -0,0 +1,8 @@
+
+select * from MarketingAnalytics.dbo.customers;
+--------------------------------------------------
+select * from MarketingAnalytics.dbo.geography;
+select c.CustomerID, c.CustomerName, c.Email, c.Gender, c.Age, g.Country, g.City 
+from MarketingAnalytics.dbo.customers as c left join MarketingAnalytics.dbo.geography as g
+on c.GeographyID = g.GeographyID
+
